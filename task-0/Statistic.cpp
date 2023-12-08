4feb7b19b741fc55eb982672ff1ec3a21ded139c
Daniil Chumakin <145126478+Pryduck@users.noreply.github.com>
2023-12-08 20:43:56 +0700

    Update Statistic.cpp

--- task-0/Statistic.cpp
+++ task-0/Statistic.cpp
@@ -15,8 +15,8 @@ static bool comp(const pair<string, int>& v1, const pair<string, int>& v2) {
 
 vector<pair<string, int>> Statistic::sorting() const {
     vector<pair<string, int>> sorted = {};                                  //создали
-    sorted.resize(stat_map.size());                                //задали размер
-    copy(stat_map.begin(), stat_map.end(), sorted.begin()); //внесли элементы
-    sort(sorted.begin(), sorted.end(), comp);                     //отсортировали
+    sorted.resize(stat_map.size());                                         //задали размер
+    copy(stat_map.begin(), stat_map.end(), sorted.begin());                 //внесли элементы
+    sort(sorted.begin(), sorted.end(), comp);                               //отсортировали
     return sorted;                                                          //вернули
 }
