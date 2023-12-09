c3bfbcab642201d5292aa998822e210d5533a816
Daniil Chumakin <145126478+Pryduck@users.noreply.github.com>
2023-12-09 11:58:51 +0700

    Update Statistic.cpp

--- task-0/Statistic.cpp
+++ task-0/Statistic.cpp
@@ -1,19 +1,19 @@
 #include "Statistic.h"
 
-void Statistic::adding(const string& word) {
+void Statistic::add(const string& word) {
     word_number++;
     stat_map[word]++;
 }
 
-int Statistic::get_number() const {
-    return word_number;
+int Statistic::getNumber() const {
+    return wordNumber;
 }
 
 static bool comp(const pair<string, int>& v1, const pair<string, int>& v2) {
     return v1.second > v2.second;
 }
 
-vector<pair<string, int>> Statistic::sorting() const {
+vector<pair<string, int>> Statistic::sort() const {
     vector<pair<string, int>> sorted = {};                                  //создали
     sorted.resize(stat_map.size());                                         //задали размер
     copy(stat_map.begin(), stat_map.end(), sorted.begin());                 //внесли элементы
