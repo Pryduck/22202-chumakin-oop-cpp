644e1331d25512e72c183b083ff38f4b07cdb536
Daniil Chumakin <145126478+Pryduck@users.noreply.github.com>
2023-12-15 22:25:02 +0700

    Update FileReader_test.cpp

--- task-0/Google_tests/FileReader_test.cpp
+++ task-0/Google_tests/FileReader_test.cpp
@@ -9,20 +9,20 @@ test_file.close();
 FileReader fileReader("poem.txt");
 fileReader.open();
 
-ASSERT_EQ(fileReader.get_next(), "Remember, remember the fifth of November,");
-ASSERT_EQ(fileReader.get_next(), "Gunpowder treason and plot,");
-ASSERT_TRUE(fileReader.is_next());
-ASSERT_EQ(fileReader.get_next(), "I see no reason why gunpowder treason");
-ASSERT_EQ(fileReader.get_next(), "Should ever be forgot");
-ASSERT_FALSE(fileReader.is_next());
+ASSERT_EQ(fileReader.getNext(), "Remember, remember the fifth of November,");
+ASSERT_EQ(fileReader.getNext(), "Gunpowder treason and plot,");
+ASSERT_TRUE(fileReader.isNext());
+ASSERT_EQ(fileReader.getNext(), "I see no reason why gunpowder treason");
+ASSERT_EQ(fileReader.getNext(), "Should ever be forgot");
+ASSERT_FALSE(fileReader.isNext());
 }
 
-TEST(FileReader,no_words) {
-std::ofstream test_file("no_words.txt");
+TEST(FileReader,NoWords) {
+std::ofstream test_file("NoWords.txt");
 test_file.close();
-FileReader fileReader("no_words.txt");
+FileReader fileReader("NoWords.txt");
 fileReader.open();
 
-ASSERT_EQ(fileReader.get_next(), "");
-ASSERT_FALSE(fileReader.is_next());
+ASSERT_EQ(fileReader.getNext(), "");
+ASSERT_FALSE(fileReader.isNext());
 }
