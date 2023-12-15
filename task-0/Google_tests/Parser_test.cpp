b0eb89a4196c76992d5fb20b0ac634cb0f61fc3d
Daniil Chumakin <145126478+Pryduck@users.noreply.github.com>
2023-12-15 22:21:14 +0700

    Update Parser_test.cpp

--- task-0/Google_tests/Parser_test.cpp
+++ task-0/Google_tests/Parser_test.cpp
@@ -1,29 +1,51 @@
 #include "gtest/gtest.h"
 #include "../Parser.h"
 using namespace std;
+
 TEST(Parser, simple_text) {
 Parser parser;
 
 string simple_text = "We gonna rapapap tonight";
-vector<string> words = parser.parsing(simple_text);
+bool sense = false;
+  
+vector<string> words = parser.parse(simple_text, sense);
 
 ASSERT_EQ(words, vector<string>({"we", "gonna", "rapapap", "tonight"}));
 }
+
+
+TEST(Parser, caseSensitive) {
+Parser parser;
+
+string caseSensitive = "We we are duper Duper";
+bool sense = true;
+  
+vector<string> words = parser.parse(caseSensitive, sense);
+
+ASSERT_EQ(words, vector<string>({"We", "we", "are", "duper", "Duper"}));
+}
+
+
 
 TEST(Parser, strange_text) {
 Parser parser;
 
 string simple_text = "We$$$are**the@Champions";
-vector<string> words = parser.parsing(simple_text);
+bool sense = false;
+  
+vector<string> words = parser.parse(simple_text);
 
 ASSERT_EQ(words, vector<string>({"we", "are", "the", "champions"}));
 }
+
 
 TEST(Parser, no_text) {
 Parser parser;
 
 string no_text;
-vector<string> words = parser.parsing(no_text);
+bool sense = false;
+  
+vector<string> words = parser.parse(no_text, sense);
 
 ASSERT_EQ(words.size(), 0);
 }
